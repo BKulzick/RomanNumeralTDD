8f6460c90d8a60296ff09973c8dee70f36a03795
BKulzick <BryanKulczycki@gmail.com>
2015-04-16 13:08:58 -0400

    first green test

---
+++ RomanNumeralTDD/src/main/RomanNumeralConverter.java
@@ -0,0 +1,11 @@
+package main;
+
+public class RomanNumeralConverter {
+
+	public String convertFromIntToRomanNumeral(int inputNumber) {
+		if (inputNumber == 1)
+			return "I";
+		return null;
+	}
+
+}
--- RomanNumeralTDD/src/test/RomanNumeralTest.java
+++ RomanNumeralTDD/src/test/RomanNumeralTest.java
@@ -1,14 +1,16 @@
 package test;
 
 import static org.junit.Assert.*;
+import main.RomanNumeralConverter;
 
 import org.junit.Test;
 
 public class RomanNumeralTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void whenGivenOneOutputShouldBeI() {
+		RomanNumeralConverter romanNumeral = new RomanNumeralConverter();
+		assertEquals("I", romanNumeral.convertFromIntToRomanNumeral(1));
 	}
 
 }
