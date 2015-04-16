d5dc45a52fea5d4cdd66b3aaf92abc0ff19c3200
BKulzick <BryanKulczycki@gmail.com>
2015-04-16 13:59:08 -0400

    NEW APPROACH! Went to a switch statment, tests still pass

--- RomanNumeralTDD/src/main/RomanNumeralConverter.java
+++ RomanNumeralTDD/src/main/RomanNumeralConverter.java
@@ -4,19 +4,41 @@ public class RomanNumeralConverter {
 
 	public StringBuilder convertFromIntToRomanNumeral(int inputNumber) {
 		StringBuilder answer = new StringBuilder();
-		if (inputNumber < 4) {
-			for (int i = 1; i <= inputNumber; i++) {
-				answer.append("I");
-			}
-		}else if (inputNumber == 4){
-			answer.append("IV");
-		}else if (inputNumber == 5){
-			 answer.append("V");
-		}else if (inputNumber == 10){
-			answer.append("X");
-		}
-		return answer;
-	
 
-}
+		switch (inputNumber) {
+
+		case 3:
+			answer.append("I");
+		case 2:
+			answer.append("I");
+		case 1:
+			answer.append("I");
+			break;
+		case 4:
+			answer.append("IV");
+			break;
+		case 5:
+			answer.append("V");
+			break;
+		case 6:
+			answer.append("VI");
+			break;
+		case 7:
+			answer.append("VII");
+			break;
+		case 8:
+			answer.append("VIII");
+			break;
+		case 9:
+			answer.append("IX");
+			break;
+		
+		case 10:
+			answer.append("X");
+			break;
+		}
+			
+		return answer;
+
 	}
+}
