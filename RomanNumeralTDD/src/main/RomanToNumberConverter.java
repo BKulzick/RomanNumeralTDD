4233392641cbc8734a150da146882b513d36114b
BKulzick <BryanKulczycki@gmail.com>
2015-04-16 15:29:56 -0400

    First Test passed

--- RomanNumeralTDD/src/main/RomanToNumberConverter.java
+++ RomanNumeralTDD/src/main/RomanToNumberConverter.java
@@ -2,4 +2,9 @@ package main;
 
 public class RomanToNumberConverter {
 
+	public int convertFromRomanNumeralToInt(String romanNumeralInput) {
+		
+		return 1;
+	}
+
 }
--- RomanNumeralTDD/src/test/RomanToNumberConverterTest.java
+++ RomanNumeralTDD/src/test/RomanToNumberConverterTest.java
@@ -1,14 +1,24 @@
 package test;
 
 import static org.junit.Assert.*;
+import main.NumberToRomanConverter;
+import main.RomanToNumberConverter;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class RomanToNumberConverterTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	RomanToNumberConverter numberConverter;
+
+	@Before
+	public void setUp() {
+		numberConverter = new RomanToNumberConverter();
 	}
 
+	@Test
+	public void whenGivenIOutputShouldBeOne() {
+		assertEquals(1,
+				numberConverter.convertFromRomanNumeralToInt("I"));
+	}
 }
